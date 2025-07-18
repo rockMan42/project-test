00c6ae10d5ce405f215ca1d24707d1318216ddbd
zhaowenxi <16629305107@163.com>
2025-07-18 17:49:22 +0800

    step_3_修改时间字段

--- src/main/java/org/example/projecttest/entity/Customer.java
+++ src/main/java/org/example/projecttest/entity/Customer.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class Customer {
 
-    private String createTime;
+
 
     private String customerName;
 }
--- src/main/java/org/example/projecttest/entity/Project.java
+++ src/main/java/org/example/projecttest/entity/Project.java
@@ -23,5 +23,5 @@ public class Project {
 
     private Product product;
 
-
+    private String createTime;
 }
